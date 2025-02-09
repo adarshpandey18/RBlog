a5804794fd358b33481cb8e7ff2f71262e2c580f
Adarsh Pandey <itsadarshvijaypandey@gmail.com>
2025-02-10 00:45:01 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'R Blog',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: null,
     );
   }
 }
